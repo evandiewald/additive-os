205dcb367465b693da9189ae4e867da4804d9b39
evandiewald <37876940+evandiewald@users.noreply.github.com>
2021-07-12 16:00:25 -0400

    updated smart contracts

--- solidity/AMLicense.sol
+++ solidity/AMLicense.sol
@@ -9,42 +9,45 @@ pragma solidity >=0.7.0 <0.9.0;
 contract AMLicense {
    
     struct Print {
-        uint timestamp;
-        uint operatorid;
+        address approvedby;
         string reporthash;
     }
 
     struct License {
         address licensedby;
-        address licensedfor;
-        string parthash;
+        address licensedto;
         uint numprints;
+        string parthash;
         Print[] prints;
     }
 
 
-    License[] public licenses;
+    License[] public licenseList;
+    mapping(uint256 => License) licenses;
     uint256 public licenseCount = 0;
+    
 
 
-    function addLicense(address _licensedby, address _licensedfor, string memory _parthash, uint _numprints) public returns (uint){
+
+    function addLicense(address _licensedto, uint _numprints, string memory _parthash) public returns (License memory){
         License storage new_license = licenses[licenseCount];
-        new_license.licensedby = _licensedby;
-        new_license.licensedfor = _licensedfor;
+        new_license.licensedby = msg.sender;
+        new_license.licensedto = _licensedto;
         new_license.parthash = _parthash;
         new_license.numprints = _numprints;
-        
-        licenses.push(new_license);
+
+        licenseList.push(new_license);
         
         licenseCount++;
         
-        return licenses.length;
+        return licenses[licenseCount-1];
     }
 
 
-    function addPrint(uint _licenseid, uint _timestamp, uint _operatorid, string memory _reporthash) public returns (uint){
-        require(licenses[_licenseid].prints.length < licenses[_licenseid].numprints, "Part quota already met for this license.");
-        Print memory new_print = Print(_timestamp, _operatorid, _reporthash);
+    function addPrint(uint _licenseid, string memory _reporthash) public returns (uint){
+        require(licenses[_licenseid].prints.length < licenses[_licenseid].numprints, "Part quota already met for this license or license ID does not exist.");
+        require((msg.sender == licenses[_licenseid].licensedby) || (msg.sender == licenses[_licenseid].licensedto), "You are not authorized to add prints to this license.");
+        Print memory new_print = Print(msg.sender, _reporthash);
         licenses[_licenseid].prints.push(new_print);
         
         return licenses[_licenseid].prints.length;
@@ -52,11 +55,24 @@ contract AMLicense {
     
     
     function getLicense(uint _licenseid) public view returns (License memory) {
+        require(_licenseid < licenseCount, "License ID does not exist.");
         return licenses[_licenseid];
     }
     
     
     function getPrint(uint _licenseid, uint _printid) public view returns (Print memory) {
+        require(_licenseid < licenseCount, "License ID does not exist.");
+        require(_printid < licenses[_licenseid].prints.length, "Print ID does not exist.");
         return licenses[_licenseid].prints[_printid];
+    }
+    
+    
+    function listLicenses() public view returns (License[] memory) {
+        return licenseList;
+    }
+    
+    
+    function getLicenseCount() public view returns (uint){
+        return licenseList.length;
     }
 }
