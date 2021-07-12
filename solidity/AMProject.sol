22aee245c2b2c7c5a78c175fe53050958ceb81dc
evandiewald <37876940+evandiewald@users.noreply.github.com>
2021-07-12 15:49:17 -0400

    updated smart contracts

--- solidity/AMProject.sol
+++ solidity/AMProject.sol
@@ -7,23 +7,26 @@ pragma solidity >=0.7.0 <0.9.0;
  * @dev Stores project metadata such as file hashes
  */
 contract AMProject {
+    
+    struct File {
+        string hash;
+        address author;
+    }
    
     struct Project {
         address author;
-        string[] files;
+        File[] files;
     }
 
 
     Project[] public projectList;
     mapping(uint256 => Project) projects;
     uint256 public projectCount = 0;
-    
 
 
-
-    function addProject(address _author) public returns (Project memory){
+    function addProject() public returns (Project memory){
         Project storage new_project = projects[projectCount];
-        new_project.author = _author;
+        new_project.author = msg.sender;
         
         projectList.push(new_project);
 
@@ -33,9 +36,10 @@ contract AMProject {
     }
 
 
-    function addHash(uint _projectid, string memory _checksum) public returns (uint){
-        
-        projects[_projectid].files.push(_checksum);
+    function addFile(uint _projectid, string memory _checksum) public returns (uint){
+        require(_projectid < projectCount, "Project ID does not exist.");
+        File memory new_file = File(_checksum, msg.sender);
+        projects[_projectid].files.push(new_file);
         
         return projects[_projectid].files.length;
     }
@@ -47,10 +51,10 @@ contract AMProject {
     }
     
     
-    function getHash(uint _projectid, uint _fileid) public view returns (string memory) {
+    function getFile(uint _projectid, uint _fileid) public view returns (File memory) {
         require(_projectid < projectCount, "Project ID does not exist.");
         require(_fileid < projects[_projectid].files.length, "File ID does not exist.");
-        return projects[_projectid].files[_fileid];
+        return projects[_projectid].files[_fileid]; // tuple(hash: string, author: address)
     }
     
     
